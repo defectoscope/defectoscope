8fbee726f9cae031ad883c69187ca5e2321be1b2
Andrey Osnovin <AndreyOsnovin@zohomail.eu>
2024-08-08 10:55:26 +0400

    Version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Defectoscope.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/defectoscope/defectoscope"
-  @version "0.1.1"
+  @version "0.1.2"
 
   def project do
     [
